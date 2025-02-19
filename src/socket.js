e76cedfe57e2e28dc4195f69d2f3c6e8e3616064
Jay Mehta <jayjmehta251203@gmail.com>
2025-02-19 06:39:25 +0530

    Socket changes made

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("chatroulletexbackend-production.up.railway.app", {
   transports: ["websocket"],
   withCredentials: true,
 });
