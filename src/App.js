03fb41656b982290a016c869670dd2f742deff24
Jay Mehta <jayjmehta251203@gmail.com>
2025-03-12 19:35:43 +0530

    Clear for Stage 7

--- src/App.js
+++ src/App.js
@@ -9,37 +9,39 @@ import ParticlesPage from "./about";
 import Register from "./Register";
 import EmailVerification from "./EmailVerification";
 import Login from "./Login";
-import Tutorial from './TutorialPage';
+import Tutorial from './TutorialPage'; // Note: You imported TutorialPage twice with different names
 import ForgotPassword from './ForgotPassword';
 import ResetPassword from './ResetPassword';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Navigate to="/Login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify-email" element={<EmailVerification />} />
-        
-        {/* Protected routes */}
-        <Route path="/tutorial" element={<Tutorial />} />
-        
-        
-        
-        {/* Main Application Routes */}
-        <Route path="/Home" element={<Home_page />} />
-        <Route path="/TutorialPage" element={<TutorialPage />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/ChatLanding" element={<ChatLanding />} />
-        <Route path="/ChatMain" element={<ChatMain />} />
-        <Route path="/about" element={<ParticlesPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Analytics />
-      </Routes>
-    </Router>
+    <div>
+      {/* Analytics should only be here, outside Routes */}
+      <Analytics />
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Navigate to="/Login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
+          
+          {/* Protected routes */}
+          <Route path="/tutorial" element={<Tutorial />} />
+          
+          {/* Main Application Routes */}
+          <Route path="/Home" element={<Home_page />} />
+          <Route path="/TutorialPage" element={<TutorialPage />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/ChatLanding" element={<ChatLanding />} />
+          <Route path="/ChatMain" element={<ChatMain />} />
+          <Route path="/about" element={<ParticlesPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
